850e5ce06ef75d7c53fa403fa5a78f67460c37df
lephuhung <lephuhung77@gmail.com>
2025-07-21 08:30:57 +0700

    update docker compose

--- src/app/components/Panel/head/Boat.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="flex flex-shrink-0 space-x-1 md:block">
-    <slot />
-  </div>
-</template>
--- src/server/api/client/index.get.ts
+++
@@ -1,6 +0,0 @@
-export default definePermissionEventHandler('clients', 'custom', ({ user }) => {
-  if (user.role === roles.ADMIN) {
-    return WireGuard.getAllClients();
-  }
-  return WireGuard.getClientsForUser(user.id);
-});
